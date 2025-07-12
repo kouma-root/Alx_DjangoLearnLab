5c28e1231e56094e25350145978758d2ce038ecb
kouma-root <parfaitkoumakang@gmail.com>
2025-07-12 22:16:44 +0100

    Adding model to django admin and customiwe the admin interface, adding a list_filter attribut

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,5 +4,6 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
+    list_filter = ['author', 'publication_year']
     
 admin.site.register(Book, BookAdmin)
