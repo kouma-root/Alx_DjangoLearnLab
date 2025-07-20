1e5217af457c39f3bc96b74e00a362d65ac15038
kouma-root <parfaitkoumakang@gmail.com>
2025-07-20 19:21:39 +0100

    Adding a new app in the django project

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,13 +2,11 @@ from .models import Author, Book, Librarian, Library
 
 
 library_name = 'NITT Lybrary'
-library = Library.objects.get(name=library_name)
-books =library.books.all()
+librari = Library.objects.get(name=library_name)
+books =librari.books.all()
 
 author_name = "John Doe"
 author = Author.objects.get(name=author_name)
 books = Book.objects.filter(author=author)
 
-
-library = Library.objects.get(name=library_name)
-Librarian.objects.get(library = library_name)
+Librarian.objects.get(library=librari)
