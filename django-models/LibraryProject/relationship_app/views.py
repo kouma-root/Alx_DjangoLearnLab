a43ceb840d5d327395d0bdf0ace63aa10191d8af
kouma-root <parfaitkoumakang@gmail.com>
2025-07-20 21:05:49 +0100

    Adding views for displaying books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ from .models import Book, Library
 def book_list(request):
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request, 'book/book_list.html', context)
+    return render(request, 'relationship_app/book_list.html', context)
 
 class library_detail(DetailView):
     model = Library
