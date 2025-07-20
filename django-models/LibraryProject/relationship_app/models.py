bfb347ff2916f7b14ddcfb89c27aa9ba5f6d7ab7
kouma-root <parfaitkoumakang@gmail.com>
2025-07-20 14:20:19 +0100

    Adding a new app in the django project

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,14 +4,28 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=200)
     
+    def __str__(self) -> str:
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete= models.CASCADE)
     
+    def __str__(self) -> str:
+        return f"({self.title} , {self.author})"
+    
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
+    
+    def __str__(self) -> str:
+        return f"({self.name} , {self.books})"
+    
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete= models.CASCADE)
+    
+    def __str__(self) -> str:
+        return f"({self.name} , {self.library})"
+    
