1ebf96895761243c78389590af304fc41c76656e
kouma-root <parfaitkoumakang@gmail.com>
2025-07-20 23:29:00 +0100

    Adding views for displaying books

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from .views import Library_detail
-
+from .views import LibraryDetailView
 urlpatterns = [
     path('', views.list_books, name='list_books'),
-    path('Library_detail/<int:pk>/', Library_detail.as_view(), name='library_detail'),
+    path('LibraryDetailView/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
     
