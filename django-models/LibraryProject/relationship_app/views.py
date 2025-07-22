ff05c9996f443a8f28162a14881baa14fffdec9d
kouma-root <parfaitkoumakang@gmail.com>
2025-07-22 20:26:30 +0100

    Adding the Django User Authentication in the app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -22,7 +22,7 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.get_object() # Access related books
         return context
-class RegisterView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name= 'relationship_app/register.html'
