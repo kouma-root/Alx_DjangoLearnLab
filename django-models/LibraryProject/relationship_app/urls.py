f7605b1db51bbe58f94e7979f75478caffbeec91
kouma-root <parfaitkoumakang@gmail.com>
2025-07-22 21:16:50 +0100

    Adding the Django User Authentication in the app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from .views import LibraryDetailView
 urlpatterns = [
     path('', views.list_books, name='list_books'),
     path('LibraryDetailView/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', views.register.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
 ]
