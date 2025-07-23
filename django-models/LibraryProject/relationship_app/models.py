640edadd294642f14f03853c5cc2681785165082
kouma-root <parfaitkoumakang@gmail.com>
2025-07-23 07:50:33 +0100

    Adding the Django role based authentication control in our app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -29,3 +32,14 @@ class Librarian(models.Model):
     def __str__(self) -> str:
         return f"({self.name} , {self.library})"
     
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    role = models.CharField(max_length=20, choices=[('admin','Admin'),('librarian','Librarian'),('member','Member')], default='member')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created :
+        UserProfile.objects.create(user = instance)
+    else :
+        instance.userprofile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .utils import is_admin, is_librarian, is_member
 from .models import Book
 from .models import Library
 # Create your views here.
@@ -31,3 +33,20 @@ class LibraryDetailView(DetailView):
 def register(request):
     form = UserCreationForm()
     return render(request, 'relationship_app/register.html')
+
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
