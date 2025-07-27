f78f630754607bc4dff1b826591706eab440e0f1
kouma-root <parfaitkoumakang@gmail.com>
2025-07-27 10:08:20 +0100

    Adding Custom User Auth in our Django project as Advance features

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,4 +8,8 @@ class BookAdmin(admin.ModelAdmin):
     
 admin.site.register(Book, BookAdmin)
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ['is_staff', 'is_active']
+admin.site.register(CustomUser, CustomUserAdmin)
