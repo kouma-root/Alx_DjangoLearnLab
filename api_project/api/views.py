db720e75d816ea7723a151cadfdf24ae74c84701
kouma-root <parfaitkoumakang@gmail.com>
2025-08-02 00:38:33 +0100

    Adding the view for the CRUD operation based on viewset to our api app

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from .serializers import BookSerializer
 from .models import Book
 
@@ -6,6 +7,13 @@ from .models import Book
 # Create your views here.
 
 class BookList(generics.ListAPIView):
+    
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    
+    
+class BookViewSet(viewsets.ModelViewSet):
     
     queryset = Book.objects.all()
     serializer_class = BookSerializer
