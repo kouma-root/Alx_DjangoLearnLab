9b9da7b224eb7c500fc2ba127b3ed7a0785666f9
kouma-root <parfaitkoumakang@gmail.com>
2025-08-02 00:57:58 +0100

    Adding the authentification with token and the permissions  to our api app

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework import permissions, authentication
 from .serializers import BookSerializer
 from .models import Book
 
@@ -15,5 +16,7 @@ class BookList(generics.ListAPIView):
     
 class BookViewSet(viewsets.ModelViewSet):
     
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
