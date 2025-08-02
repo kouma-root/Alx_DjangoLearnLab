d941e79e36c9daec2a4a5d7d5ae25198fd49342f
kouma-root <parfaitkoumakang@gmail.com>
2025-08-02 01:03:18 +0100

    Adding the authentification with token and the permissions  to our api app

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
