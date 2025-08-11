c7b20aee606d0c08cb5ec921b52df3f3cd154a47
kouma-root <parfaitkoumakang@gmail.com>
2025-08-11 08:39:50 +0100

    Solving bugs in the advanced DRF features directory

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    # Author's full name
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    # Title of the book
+    title = models.CharField(max_length=255)
+    # Year published
+    publication_year = models.IntegerField()
+    # Relationship: One author can have many books
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
