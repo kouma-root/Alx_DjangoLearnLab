e88f7d06b856c9a241cc9f6353453d4a3fd3830c
kouma-root <parfaitkoumakang@gmail.com>
2025-08-16 13:41:09 +0100

    Initializing project and implementingpost management views and templates for task 2 Week 14

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 
 #Extend usercreationform with email
 
@@ -25,7 +30,7 @@ def register(request):
         
     else :
         form = CustomUserCreationForm()
-        return render(request, 'blog/register.html', {'form': form})
+        return render(request, 'blog/register.html', context= {'form': form})
     
     
 
@@ -39,3 +44,56 @@ def profile(request):
     return render(request, 'blog/profile.html')
 
 
+
+#Blog views management
+
+#List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+    
+    
+#view one post
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    
+
+#create post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form: forms.BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+#update post
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fileds = ['title', 'content']
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form: forms.BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self) :
+        post = self.get_object()
+        return self.request.user == post.author
+    
+# Delete post (only author can delete)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
