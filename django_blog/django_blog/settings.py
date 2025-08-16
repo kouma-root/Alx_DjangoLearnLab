062aaa569ab41fee9e1043f19a913036c2bcd5eb
kouma-root <parfaitkoumakang@gmail.com>
2025-08-16 12:51:43 +0100

    Initializing project for task 0 Week 14

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
